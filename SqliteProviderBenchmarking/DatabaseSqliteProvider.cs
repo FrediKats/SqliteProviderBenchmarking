94be3dfa46c668684dffc78d59dd658c3d8f9061
Fredi Kats <fredikats@outlook.com>
2022-05-22 16:20:03 +0300

    Add project files.

---
+++ SqliteProviderBenchmarking/DatabaseSqliteProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
+
+namespace SqliteProviderBenchmarking
+{
+    public sealed class DatabaseSqliteProvider
+    {
+        private readonly string _query;
+
+        public DatabaseSqliteProvider()
+        {
+            _query = File.ReadAllText("CreateDb.sql");
+        }
+
+        public void InitWithSqlite()
+        {
+            var sqliteConnection = new SQLiteConnection($"Data Source=:memory:");
+            sqliteConnection.Open();
+
+            using var createCommand = new SQLiteCommand(_query, sqliteConnection);
+            createCommand.ExecuteNonQuery();
+        }
+
+        public void InitWithMsSqlite()
+        {
+            var sqliteConnection = new SqliteConnection($"Data Source=:memory:");
+            sqliteConnection.Open();
+
+            using var createCommand = new SqliteCommand(_query, sqliteConnection);
+            createCommand.ExecuteNonQuery();
+        }
+    }
+}
---
+++ SqliteProviderBenchmarking/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using SqliteProviderBenchmarking;
+
+var databaseSqliteProvider = new DatabaseSqliteProvider();
+
+var stopwatch2 = Stopwatch.StartNew();
+databaseSqliteProvider.InitWithMsSqlite();
+stopwatch2.Start();
+Console.WriteLine($"MS: {stopwatch2.Elapsed}");
+
+var stopwatch = Stopwatch.StartNew();
+databaseSqliteProvider.InitWithSqlite();
+stopwatch.Start();
+Console.WriteLine($"Not ms: {stopwatch.Elapsed}");
